734fdd20f6cd5722faaa7aea29192e7e5dbfe4e8
Praveen R <praveencse2018@gmail.com>
2026-03-06 13:40:53 +0530

    commit : 06-03-2026
    1.commit for jwt token filter changes.

--- src/main/java/com/secureauth/lab/configurations/JwtFilter.java
+++ src/main/java/com/secureauth/lab/configurations/JwtFilter.java
@@ -1,8 +1,11 @@
 package com.secureauth.lab.configurations;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,29 +17,42 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private JwtService jwtService;
+	@Autowired
+	private JwtService jwtService;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
-                                    throws ServletException, IOException {
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		String path = request.getServletPath();
+		if (path.startsWith("/auth/")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		String authHeader = request.getHeader("Authorization");
 
-        String authHeader = request.getHeader("Authorization");
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return;
+		}
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+		String token = authHeader.substring(7);
 
-            String token = authHeader.substring(7);
+		try {
+			jwtService.validateToken(token);
 
-            try {
-                jwtService.validateToken(token);
-            } catch (Exception e) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
-        }
+			String username = jwtService.extractUsername(token);
 
-        filterChain.doFilter(request, response);
-    }
+			UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null,
+					new ArrayList<>());
+
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		} catch (Exception e) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return;
+		}
+
+		filterChain.doFilter(request, response);
+
+	}
 }
--- src/main/java/com/secureauth/lab/controller/AuthenticationController.java
+++ src/main/java/com/secureauth/lab/controller/AuthenticationController.java
@@ -1,65 +1,30 @@
 package com.secureauth.lab.controller;
 
-import java.util.List;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.secureauth.lab.configurations.JwtConfiguration;
 import com.secureauth.lab.configurations.JwtService;
-import com.secureauth.lab.entity.User;
 import com.secureauth.lab.repository.UserRepository;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api")
 public class AuthenticationController {
 	private UserRepository userRepo;
 	private PasswordEncoder encoder;
 	private JwtService jwtService;
 
-	public AuthenticationController(UserRepository userRepo, PasswordEncoder encoder,
-			JwtService jwtService) {
+	public AuthenticationController(UserRepository userRepo, PasswordEncoder encoder, JwtService jwtService) {
 		super();
 		this.userRepo = userRepo;
 		this.encoder = encoder;
 		this.jwtService = jwtService;
 	}
 
-	@GetMapping("/")
-	public String healthCheck() {
-		return "I am fine..!";
-	}
-
-	@PostMapping("/register")
-	public String register(@RequestBody User user) {
-		user.setPassword(encoder.encode(user.getPassword()));
-		userRepo.save(user);
-		return "User registered";
-	}
-
-	@PostMapping("/login")
-	public String login(@RequestBody User user) {
-
-//		List<User> users = userRepo.login(user.getUsername(), user.getPassword());
-		List<User> users = userRepo.login(user.getUsername());
-		if (users.isEmpty())
-			return "Invalid credentials";
-
-		User sgUser = users.get(0);
-		System.out.println(sgUser.getPassword());
-		System.out.println(encoder.encode(user.getPassword()));
-		if (sgUser == null || !encoder.matches(user.getPassword(), sgUser.getPassword()))
-			return "Invalid credentials";
-		return jwtService.generateToken(user.getUsername());
-	}
-
 	@PostMapping("/jwt-test")
-	public String testJwt() {
+	public String testJwt(@RequestHeader String authorization) {
 		return "jwt validated";
 	}
 }
